befde6fac8a73b6f7381287d09affca68c46e7b1
Filip Masiukiewicz <scynthero@gmail.com>
2022-05-11 21:20:18 +0200

    feat: deconstruct, send and reconstruct ndarray

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 #![allow(dead_code)]
-
+#![allow(unused)]
 extern crate mpi;
 
-use ndarray::{arr2, Array2, ArrayView, Dim, s};
-use ndarray::prelude;
+use ndarray::{arr2, Array, Array2, ArrayView, Dim, s};
 
 use mpi::traits::*;
 
@@ -14,7 +13,6 @@ fn main() {
     let rank = world.rank();
 
     if rank == 0 {
-        let mut b_slices: Vec<Array2<i32>> = vec![];
         let p_sqrt = size.sqrt() as i32;
 
         let a = arr2(&[
@@ -34,35 +32,47 @@ fn main() {
             [4, 0, -8, 5, 3, 1],
             [4, 0, -8, 5, 3, 1],
         ]);
-        let dims = a.dim().0 as i32;
-        println!("{:} {:}", dims, p_sqrt);
+        println!("Hello from rank init");
         let a_slices = split(&a, p_sqrt);
-        println!("{:?}", a_slices);
+        let b_slices = split(&b, p_sqrt);
+        // println!("{:?}", a_slices);
+        // println!("{:}?", a_slices[0].dot(&a_slices[1]))
+        a_slices.iter().enumerate().for_each(|(idx, item)| {
+            println!("{:?}", item.to_owned().as_slice().unwrap());
+            world.process_at_rank(idx as i32).send(item.to_owned().as_slice().unwrap());
+        })
+    } else {
+        let (mut msg, _status) = world.process_at_rank(0).receive_vec::<i32>();
+        let new_arr = Array2::from_shape_vec((3, 3), msg.to_vec()).unwrap();
+        println!("Hello from rank {:} {:?}", world.rank(), new_arr);
     }
 
-    let c = arr2(&[
-        [33, 52, 26, -14],
-        [53, 44, 2, 57],
-        [82, 55, 30, 25],
-        [57, 96, 82, -7],
-    ]);
+    world.barrier();
+
+    println!("Wololo")
+    // let c = arr2(&[
+    //     [33, 52, 26, -14],
+    //     [53, 44, 2, 57],
+    //     [82, 55, 30, 25],
+    //     [57, 96, 82, -7],
+    // ]);
 
     // let calculated_c = a.dot(&b);
     // println!("{}", calculated_c.slice(s![0..2,0..2]));
     // assert_eq!(c, calculated_c)
 }
 
-fn split(matrix: &Array2<i32>, parts: i32) -> Vec<ArrayView<i32, Dim<[usize; 2]>>>{
+fn split(matrix: &Array2<i32>, parts: i32) -> Vec<ArrayView<i32, Dim<[usize; 2]>>> {
     let dim_x = matrix.dim().0 as i32;
     let dim_y = matrix.dim().1 as i32;
     let mut sliced = vec![];
 
-    for i in (0..parts){
-            for j in (0..parts) {
-                sliced.push(matrix.slice(
-                    s![i*(dim_x/parts)..i*(dim_x/parts)+(dim_x/parts),
+    for i in 0..parts {
+        for j in 0..parts {
+            sliced.push(matrix.slice(
+                s![i*(dim_x/parts)..i*(dim_x/parts)+(dim_x/parts),
                     j*(dim_y/parts)..j*(dim_y/parts)+(dim_y/parts)]));
-            }
         }
+    }
     sliced
 }
